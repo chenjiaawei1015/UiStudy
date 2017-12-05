54d17cea60ccbfb6bb3e5ad579d3fb80488be201
chenjiawei <chenjiawei@iboxpay.com>
2017-12-05 09:22:39 +0800

    所有代码进行压缩

--- day04/code/Demo3_RollText/app/src/main/java/com/cjw/demo3_rolltext/IHandleMessage.java
+++
@@ -1,9 +0,0 @@
-package com.cjw.demo3_rolltext;
-
-
-import android.os.Message;
-
-public interface IHandleMessage {
-
-    void handleMessage(Message msg);
-}
--- day04/code/Demo4_RadarScan/app/src/main/java/com/cjw/demo4_radarscan/IHandleMessage.java
+++
@@ -1,9 +0,0 @@
-package com.cjw.demo4_radarscan;
-
-
-import android.os.Message;
-
-public interface IHandleMessage {
-
-    void handleMessage(Message msg);
-}
